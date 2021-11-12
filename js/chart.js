76687fdb9c0f4015c7b3d9273b5bb6f1f53fd140
cheekezie <harrychikezie1@gmail.com>
2021-11-12 15:41:54 +0100

    dashboard page implemented

---
+++ js/chart.js
@@ -0,0 +1,35 @@
+const ctx = document.getElementById('dashboardSummaryChart');
+const myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN'],
+        datasets: [{
+            label: '',
+            data: [300, 250, 500, 200, 450, 300],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
