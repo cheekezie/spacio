4f3a635fa48b3f94fce65014bcc8cbaf1a896190
cheekezie <harrychikezie1@gmail.com>
2021-11-13 18:34:46 +0100

    chart updated

--- js/chart.js
+++ js/chart.js
@@ -1,35 +1,56 @@
 const ctx = document.getElementById('dashboardSummaryChart');
+Chart.defaults.font.size = 14;
 const myChart = new Chart(ctx, {
     type: 'bar',
     data: {
         labels: ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN'],
         datasets: [{
-            label: '',
             data: [300, 250, 500, 200, 450, 300],
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+                'rgba(27, 117, 188, 0.35)',
+                'rgba(27, 117, 188, 0.35)',
+                'rgba(27, 117, 188, 0.35)',
+                'rgba(27, 117, 188, 0.35)',
+                'rgba(27, 117, 188, 0.35)',
+                '#1B75BC'
             ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
+            // borderColor: [
+            //     'rgba(27, 117, 188, 0.35)',
+            //     'rgba(27, 117, 188, 0.35)',
+            //     'rgba(27, 117, 188, 0.35)',
+            //     'rgba(27, 117, 188, 0.35)',
+            //     'rgba(27, 117, 188, 0.35)',
+            //     '#1B75BC'
+            // ],
+            borderWidth: 0
         }]
     },
     options: {
-        scales: {
-            y: {
-                beginAtZero: true
+        plugins: {
+            legend: {
+                display: false,
             }
+        },
+        maxBarThickness: 25,
+        scales: {
+            x: {
+                beginAtZero: true,
+                grid: {
+                    display: false
+                },
+                ticks: {
+                    color: '#888888',
+                }
+            },
+            y: {
+                beginAtZero: true,
+                grid: {
+                    display: false
+                },
+                ticks: {
+                    color: '#888888',
+                }
+            },
         }
     }
 });
