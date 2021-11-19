7fd3767c8a5b8be215f009933d111ccabfe9d7b8
cheekezie <harrychikezie1@gmail.com>
2021-11-19 15:57:26 +0100

    updated button routings

--- js/main.js
+++ js/main.js
@@ -1,6 +1,32 @@
 'use strict';
-
- 
+$("#signin").click(function(){
+    window.location.href =
+    '/pages/dashboard-summary.html';
+});
+$("#business").click(function(){
+    window.location.href =
+    '/onboarding/success.html';
+});
+$("#personal").click(function(){
+    window.location.href =
+    '/onboarding/otp.html';
+});
+$("#otpBtn").click(function(){
+    window.location.href =
+    '/onboarding/finish.html';
+});
+$("#finish").click(function(){
+    window.location.href =
+    '/onboarding/signin.html';
+});
+$("#onboardManagers").click(function(){
+    window.location.href =
+    '/onboarding/managers-business.html';
+});
+$("#onboardResidents").click(function(){
+    window.location.href =
+    '/onboarding/managers-signup.html';
+});
 // var input = document.querySelector("#telephone");
 // window.intlTelInput(input, {
 //     allowDropdown:true,
