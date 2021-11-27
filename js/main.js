31218bd44503426797196293ee81dbb10bf85940
cheekezie <harrychikezie1@gmail.com>
2021-11-27 21:06:20 +0100

    icons updated

--- js/main.js
+++ js/main.js
@@ -1,44 +1,51 @@
 'use strict';
-$("#signin").click(function(){
-    window.location.href =
-    '/pages/dashboard-summary.html';
-});
-$("#business").click(function(){
-    window.location.href =
-    '/onboarding/success.html';
-});
-$("#personal").click(function(){
-    window.location.href =
-    '/onboarding/otp.html';
-});
-$("#otpBtn").click(function(){
-    window.location.href =
-    '/onboarding/finish.html';
-});
-$("#finish").click(function(){
-    window.location.href =
-    '/onboarding/signin.html';
-});
-$("#onboardManagers").click(function(){
-    window.location.href =
-    '/onboarding/managers-business.html';
-});
-$("#onboardResidents").click(function(){
-    window.location.href =
-    '/onboarding/managers-signup.html';
-});
-$("#success").click(function(){
-    window.location.href =
-    '/onboarding/otp.html';
-});
-$(".propertyDetail").click(function(){
-    window.location.href =
-    '/pages/property-listings.html';
-});
-$(".noticeDetails").click(function(){
-    console.log('kkkk');
-    window.location.href =
-    '/pages/demand-notice-listings.html';
+$(document).ready(function () {
+  $('#signin').click(function () {
+    window.location.href = '/pages/dashboard-summary.html';
+  });
+  $('#business').click(function () {
+    window.location.href = '/onboarding/success.html';
+  });
+  $('#personal').click(function () {
+    window.location.href = '/onboarding/otp.html';
+  });
+  $('#otpBtn').click(function () {
+    window.location.href = '/onboarding/finish.html';
+  });
+  $('#finish').click(function () {
+    window.location.href = '/onboarding/signin.html';
+  });
+  $('#onboardManagers').click(function () {
+    window.location.href = '/onboarding/managers-business.html';
+  });
+  $('#onboardResidents').click(function () {
+    window.location.href = '/onboarding/managers-signup.html';
+  });
+  $('#success').click(function () {
+    window.location.href = '/onboarding/otp.html';
+  });
+  $('.propertyDetail').click(function () {
+    window.location.href = '/pages/property-listings.html';
+  });
+  $('.noticeDetails').click(function () {
+    window.location.href = '/pages/demand-notice-listings.html';
+  });
+
+
+  // ClICK OUTSIDE TO HIDE SIDEMENU AND OVERLAY
+  $(document).mouseup((e) => {
+    const container = $('#dialog');
+    const toggler = $('.dialog-content');
+    // If the target of the click isn't the dialog container and content
+    if (
+      !container.is(e.target) &&
+      !toggler.is(e.target) &&
+      container.has(e.target).length === 0
+    ) {
+      const dialog = document.getElementById("myDialog");
+      dialog.style.display = "none";
+    }
+  });
 });
 
 // var input = document.querySelector("#telephone");
--- js/page-loader.js
+++ js/page-loader.js
@@ -1,15 +1,38 @@
 'use strict';
+class Menu extends HTMLElement {
+  constructor() {
+    super();
+  }
+  connectedCallback() {
+    // To get the menu component and attach to current page
+    fetch('../partials/menu.html')
+      .then((response) => {
+        return response.text();
+      })
+      .then((data) => {
+        this.innerHTML = data;
+        $('#addNewBtn').click(function () {
+          const dialog = document.getElementById("myDialog");
+          dialog.style.display = "block";
+        });
+      });
+  }
+}
+class AddPopup extends HTMLElement {
+  constructor() {
+    super();
+  }
+  connectedCallback() {
+    // To get the popup component and attach to current page
+    fetch('../partials/add-popup.html')
+      .then((response) => {
+        return response.text();
+      })
+      .then((data) => {
+        this.innerHTML = data;
+      });
+  }
+}
 
-// To get the menu component and attach to current page
-fetch("../partials/menu.html")
-  .then(response => {
-    return response.text()
-  })
-  .then(data => {
-      document.querySelector("navigation").innerHTML = data;
-        // const template = document.getElementById('summary');
-        // const page = document.getElementById('outlet');
-        // page.appendChild(
-        //   document.importNode(template.content, true)
-        // );
-  });
+customElements.define('new-item-popup-component', AddPopup);
+customElements.define('navigation-component', Menu);
